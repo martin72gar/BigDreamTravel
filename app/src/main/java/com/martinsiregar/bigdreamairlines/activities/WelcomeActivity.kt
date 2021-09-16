94915e04507ffbde2450ca34cb8ee31ec22eeec6
Office PC <72garmartin@gmail.com>
2021-09-16 17:03:51 +0700

    add exported=true to manifest, get message from another server

--- app/src/main/java/com/martinsiregar/bigdreamairlines/activities/WelcomeActivity.kt
+++ app/src/main/java/com/martinsiregar/bigdreamairlines/activities/WelcomeActivity.kt
@@ -3,23 +3,46 @@ package com.martinsiregar.bigdreamairlines.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.martinsiregar.bigdreamairlines.R
+import com.martinsiregar.bigdreamairlines.services.MessageService
+import com.martinsiregar.bigdreamairlines.services.ServiceBuilder
 import kotlinx.android.synthetic.main.activity_welcome.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class WelcomeActivity : AppCompatActivity() {
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_welcome)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
 
-		// To be replaced by retrofit code
-		message.text = "Black Friday! Get 50% cash back on saving your first spot."
-	}
+        val messageService = ServiceBuilder.buildService(MessageService::class.java)
+        val requestCall = messageService.getMessages("http://192.168.0.197:7000/messages")
 
-	fun getStarted(view: View) {
-		val intent = Intent(this, DestinationListActivity::class.java)
-		startActivity(intent)
-		finish()
-	}
+        requestCall.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    val msg = response.body()
+                    msg?.let {
+                        message.text = msg
+                    }
+                } else {
+                    Toast.makeText(this@WelcomeActivity, "Failed to retrieve item.", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Toast.makeText(this@WelcomeActivity, "Failed to retrieve item.", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    fun getStarted(view: View) {
+        val intent = Intent(this, DestinationListActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
